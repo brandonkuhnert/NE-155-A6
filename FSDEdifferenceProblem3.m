1966d7914d200b113f9b0735cd769469d3f0ecb1
brandonkuhnert <brandonkuhnert@berkeley.edu>
2016-04-11 08:37:43 -0700

    Added files via upload

---
+++ FSDEdifferenceProblem3.m
@@ -0,0 +1,45 @@
+function [ x,flux,n,A ] = FSDEdifferenceProblem3( a,h,D,Sab,S )
+%Brandon Kuhnert
+%Inputs: a boundary conditions, h mesh size, D diffusion constant,
+%Sab macroscopic absorption xsec, S source function
+%Output: x corresponding x's to flux valuses, flux vector
+
+L=sqrt(D/Sab); %diffusion length
+n=2*a/h;
+x= (-a+h):h:(a-h);
+
+%Build the matrix and vector to solve
+
+mdiag=((2*D/(h^2))+Sab).*ones(n,1);
+sdiag=((-D)/(h^2)).*ones(n-1,1);
+A=diag(mdiag)+diag(sdiag,-1)+diag(sdiag,1);
+
+s=zeros(n-1,1);
+for k=1:(n-1)
+    s(k)=S(x(k));
+end
+
+%Solve using Thomas Algorithm
+%define constants
+
+N=length(s);
+t=zeros(N,1);
+y=t;
+d=diag(A);
+q=diag(A,1);
+a=diag(A,-1);
+w=d(1);
+y(1)=s(1)/w;
+
+for i=2:N
+    t(i-1)=q(i-1)/w;
+    w=d(i)-a(i-1)*t(i-1);
+    y(i)=(s(i)-a(i-1)*y(i-1))/w;
+end
+for j=(N-1):(-1):1
+    y(j)=y(j)-(t(j)*y(j+1));
+end
+
+flux=y;
+
+end
